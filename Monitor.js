c3feae9aef0f7e82371160ad0406a23083c74f57
Stas Ukolov <ukoloff@gmail.com>
2013-03-05 15:32:40 +0600

    Import some code

--- Monitor.js
+++ Monitor.js
@@ -8,3 +8,113 @@ var Hosts=[
 {ip: '192.168.16.3', name: 'Три'}
 ];
 
+var $={};
+
+//goW();
+$.doc=newDoc();
+startPage();
+
+//--[Functions]
+
+// Перезапуститься в wscript (убрать консоль)
+function goW()
+{
+ WScript.Interactive=false;
+
+ if(WScript.FullName.replace(/^.*[\/\\]/, '').match(/^w/i)) return;
+ (new ActiveXObject("WScript.Shell")).Run('wscript //B "'+
+    WScript.ScriptFullName+'"', 0, false);
+ WScript.Quit();
+}
+
+// Переделать спецсимволы в HTML-коды
+function html(S)
+{
+ S=''+S;
+ var E={'&':'amp', '>':'gt', '<':'lt', '"':'quot'};
+ for(var Z in E)
+   S=S.split(Z).join('&'+E[Z]+';');
+ return S;
+}
+
+// Открыть MSIE
+function newDoc()
+{
+ var ie=WScript.CreateObject('InternetExplorer.Application');
+ ie.AddressBar=false;
+ ie.StatusBar=false;
+ ie.ToolBar=false;
+ ie.MenuBar=false;
+ ie.Visible=true;
+ ie.Navigate('about:blank');
+ while(ie.Busy) WScript.Sleep(100);
+ $.ie=ie;
+ return ie.Document;
+}
+
+// Открыть стартовую страницу
+function startPage()
+{
+ $.doc.open();
+ $.doc.write(readSnippet('html'));
+ $.doc.close();
+
+ $.window=$.doc.parentWindow;
+ $.doc.body.onunload=function(){ $.closed=1; }
+ $.interior=$.doc.getElementById('Interior');
+}
+
+// Выделить кусочек текста из исходного кода
+function readSnippet(name)
+{
+ var f=WScript.CreateObject("Scripting.FileSystemObject").
+    OpenTextFile(WScript.ScriptFullName, 1);	//ForReading
+ var on, R='';
+ while(!f.AtEndOfStream)
+ {
+  var s=f.ReadLine();
+  if(!on)
+  {
+   if(s.match(/^\s*\/\*[-\s]*\[([.\w]+)\][-\s]+$/i) && (RegExp.$1==name)) on=1;
+   continue;
+  }
+  if(s.match(/^[-\s]+\*\/\s*$/)) break;
+  R+=s+'\n';
+ }
+ f.Close();
+ return R;
+}
+
+//--[Snippets]
+
+/*--[html]-----------------------------------------------------------
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<title>Монитор доступности сети</title>
+
+<style>
+
+body	{
+ background:	#A0C0E0;
+ margin:	0;
+ padding:	0.3ex;
+ color:		black;
+ font-family:	Verdana, Arial, sans-serif;
+ text-align:	justify;
+}
+
+H1	{
+ text-align:	right;
+}
+
+</style>
+</head><body>
+<H1>Монитор доступности сети</H1>
+
+<Div id='Interior'></Div>
+
+</body></html>
+-------------------------------------------------------------------*/
+
+//--[EOF]------------------------------------------------------------
